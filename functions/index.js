9f91805e83fea7fd38b14b764e802bc0404c182d
Terje Christensen <terje@christensen.no>
2019-02-07 07:47:54 +0100

    env testing

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
    
 exports.gitFunction = functions.https.onRequest((request, response) => {
-    var text = "Hello";
+    var text = "Hello!!! ";
     var text_from_env = process.env.HELLO_TXT;
     text = text + "From .env: " + text_from_env+ " :";
     response.send(text + " Debugging locally !!. Maintained in vscode, pushed to github, mirrored to goole repository, deployed to cloud functions");
