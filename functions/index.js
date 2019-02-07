37611db7149c2d0d317b1d31f28b6d15c23da05f
Terje Christensen <terje@christensen.no>
2019-02-07 12:29:26 +0100

    pretty print env

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ exports.gitFunction = functions.https.onRequest((request, response) => {
     var text = "Hello!!! ";
     var text_from_env = process.env.HELLO_TXT;
     var home = process.env.HOME;
-    var env_vars = (JSON.stringify(process.env));
+    var env_vars = JSON.stringify(process.env,null,2);
     text = text + "process.env.HELLO_TXT: (" + text_from_env+ ") : Home is : (" + home +") ALL env is : " + env_vars;
     response.send(text + " Testing env");
    });
