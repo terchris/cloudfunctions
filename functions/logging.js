54fddd0ae2fcf94865e4a1607776bf2586fadfe4
Terje Christensen <terje@christensen.no>
2019-02-12 09:41:25 +0100

    testing winston locally

---
+++ functions/logging.js
@@ -0,0 +1,44 @@
+/** Just to test logging
+ * Example from https://thisdavej.com/using-winston-a-versatile-logging-library-for-node-js/
+ */
+'use strict';
+const { createLogger, format, transports } = require('winston');
+const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const filename = path.join(logDir, 'results.log');
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    new transports.File({ filename })
+  ]
+});
+
+logger.info('Hello world');
+logger.warn('Warning message');
+logger.debug('Debugging info');
